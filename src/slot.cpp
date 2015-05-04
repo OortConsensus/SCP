e590b5d6310b7da8cf836705eceff4c57b90b008
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2015-05-03 22:27:26 -0400

    explicitly break when quorum slice full

--- src/slot.cpp
+++ src/slot.cpp
@@ -21,12 +21,12 @@ std::shared_ptr<FinishMessage> Slot::Finish() {
 
 
 void Slot::lastDefined(NodeID from, std::shared_ptr<Message>* last) {
-      try {
-        *last = M.at(from);
-      }catch(std::out_of_range){
-        *last = std::make_shared<PrepareMessage>(from, 0, Ballot{}, Ballot{}, Ballot{}, Ballot{}, Quorum{});
-        M[from] = *last;
-      }
+  try {
+    *last = M.at(from);
+  }catch(std::out_of_range){
+    *last = std::make_shared<PrepareMessage>(from, 0, Ballot{}, Ballot{}, Ballot{}, Ballot{}, Quorum{});
+    M[from] = *last;
+  }
   
 }
 void Slot::handle(std::shared_ptr<Message> _msg){
@@ -65,125 +65,133 @@ void Slot::handle(std::shared_ptr<Message> _msg){
 void Slot::handle(std::shared_ptr<PrepareMessage> msg) {
   printf("PREPARE\n");
   bool returnNow = false;
-  if (phi == PREPARE ) {
+  // If phase is not prepare, return
+  if (phi != PREPARE ) {
+    return;
+  }
 
-    // Definition -- vote:
-    //  node v votes for a iff
-    //  1) v asserts a is valid/consistent
-    //  2) v promises not to vote against a.
+  // Definition -- vote:
+  //  node v votes for a iff
+  //  1) v asserts a is valid/consistent
+  //  2) v promises not to vote against a.
 
-    // First case: We've never voted for anything. I.E. b = 0;
-    // Vote for b but don't accept yet.
-    if (state.b == Ballot {}) {
-      state.b.value = msg->b.value;
-      state.b.num = 1;
-      // Send out vote for b.
-      node->SendMessage(Prepare());
-      return;
-    }
+  // First case: We've never voted for anything. I.E. b = 0;
+  // Vote for b but don't accept yet.
+  if (state.b == Ballot {}) {
+    state.b.value = msg->b.value;
+    state.b.num = 1;
+    // Send out vote for b.
+    node->SendMessage(Prepare());
+    return;
+  }
 
-    // if( true /* && a message allows v to accept that new ballots are prepared by either of accepts 2 criteria */) {
-    // if prepared ballot then set p
+  // if( true /* && a message allows v to accept that new ballots are prepared by either of accepts 2 criteria */) {
+  // if prepared ballot then set p
     
-    // Definition -- Accept:
-    //  node v accepts statement a (the value in b) iff it has never accepted
-    //  a contradicting statment and
-    //  1) there exists U s.t. node v is in U and everyone in U has voted for
-    //    or accepted a. OR
-    //  2) Each member of a v-blocking set claims to accept a.
+  // Definition -- Accept:
+  //  node v accepts statement a (the value in b) iff it has never accepted
+  //  a contradicting statment and
+  //  1) there exists U s.t. node v is in U and everyone in U has voted for
+  //    or accepted a. OR
+  //  2) Each member of a v-blocking set claims to accept a.
     
-    // Check that we haven't accepted a contradicting statement.
+  // Check that we haven't accepted a contradicting statement.
     
-    // NOTE : the > operator does not accomplish the logic below.
-    if (compatible(msg->b, state.p)){
-      // Now check that one of our quorum slices has all voted for or 
-      // accepted b.
-      auto b_voted_or_accepted = 0;
-      for (auto kp : M) {
-        auto m = kp.second;
-        switch (m->type()) {
-        case FinishMessage_t:
-          if ((std::static_pointer_cast<FinishMessage>(m))->b == msg->b){
-            b_voted_or_accepted +=1;
-          }
-          break;
-        case PrepareMessage_t:
-          if ( (std::static_pointer_cast<PrepareMessage>(m))->b == msg->b ||
-               (std::static_pointer_cast<PrepareMessage>(m))->p == msg->b){
-            b_voted_or_accepted +=1;
-          }
-          break;
+  // NOTE : the > operator does not accomplish the logic below.
+  if (compatible(msg->b, state.p)){ 
+    // Now check that one of our quorum slices has all voted for or 
+    // accepted b.
+    auto b_voted_or_accepted = node->quorumSet.threshold;
+    for (auto kp : M) {
+      auto m = kp.second;
+      switch (m->type()) {
+      case FinishMessage_t:
+        if ((std::static_pointer_cast<FinishMessage>(m))->b == msg->b){
+          b_voted_or_accepted--;
         }
+        break;
+      case PrepareMessage_t:
+        if ( (std::static_pointer_cast<PrepareMessage>(m))->b == msg->b ||
+             (std::static_pointer_cast<PrepareMessage>(m))->p == msg->b){
+          b_voted_or_accepted--;
+        }
+        break;
       }
-
-      if (b_voted_or_accepted > node->quorumSet.threshold) {
+      // This can be moved outside of the for loop -- this let's it
+      // duck out as soon as threshold crossed
+      if ( b_voted_or_accepted == -1 ) {
         state.p = state.b;
         returnNow = true;
+        break;
       }
-    } else {
-      // Statement contradicted. Check for v-blocking.
-      auto b_vblock_vote = 0;
-      for (auto kp : M) {
-        auto m = kp.second;
-        switch (m->type()) {
-        case FinishMessage_t:
-          if ((std::static_pointer_cast<FinishMessage>(m))->b == msg->b){
-            b_vblock_vote +=1;
-          }
-          break;
-        case PrepareMessage_t:
-          if ((std::static_pointer_cast<PrepareMessage>(m))->p == msg->b){
-            b_vblock_vote +=1;
-          }
-          break;
+    }
+
+  } else {
+    // Statement contradicted. Check for v-blocking.
+    auto b_vblock_vote = node->quorumSet.threshold;
+    for (auto kp : M) {
+      auto m = kp.second;
+      switch (m->type()) {
+      case FinishMessage_t:
+        if ((std::static_pointer_cast<FinishMessage>(m))->b == msg->b){
+          b_vblock_vote--;
         }
+        break;
+      case PrepareMessage_t:
+        if ((std::static_pointer_cast<PrepareMessage>(m))->p == msg->b){
+          b_vblock_vote--;
+        }
+        break;
       }
 
-      if (b_vblock_vote > node->quorumSet.threshold) {
+      if (b_vblock_vote  == -1) {
         // v-blocking set found so vote the contradicting ballot.
         state.p_ = state.p;
         state.p = Ballot{};
         state.b.value = msg->b.value;
         state.b.num += 1;
         returnNow = true;
-      }
-    }
-
-    // If a c ballot exists but p >!~ c or p_ >!~ c, clear c.
-    if (state.c.num != 0 && (state.p > state.c || state.p_ > state.c)) {
-      state.c = Ballot{};
-      returnNow = true;
-    }
-
-    if (returnNow) {
-      node->SendMessage(Prepare());
-    }
-
-    if ( state.b != state.c && state.b == state.p /* V confirms b is prepared */ ) {
-      auto b_prepared = 0;
-      for(auto kp : M) {
-        auto m = kp.second;
-        switch (m->type()){
-        case FinishMessage_t:
-          if ((std::static_pointer_cast<FinishMessage>(m))->b == state.p){
-            b_prepared +=1;
-          }
-          break;
-        case PrepareMessage_t:
-          if ( (std::static_pointer_cast<PrepareMessage>(m))->b == state.p){
-            b_prepared +=1;
-          }
-          break;
-        }
-      }
-
-      if (b_prepared > node->quorumSet.threshold){
-        state.c = state.b;
-        node->SendMessage(Finish());
+        break;
       }
     }
 
   }
+
+  // If a c ballot exists but p >!~ c or p_ >!~ c, clear c.
+  if (state.c.num != 0 && (state.p > state.c || state.p_ > state.c)) {
+    state.c = Ballot{};
+    returnNow = true;
+  }
+
+  if (returnNow) {
+    node->SendMessage(Prepare());
+  }
+
+  if ( state.b != state.c && state.b == state.p /* V confirms b is prepared */ ) {
+    auto b_prepared = 0;
+    for(auto kp : M) {
+      auto m = kp.second;
+      switch (m->type()){
+      case FinishMessage_t:
+        if ((std::static_pointer_cast<FinishMessage>(m))->b == state.p){
+          b_prepared +=1;
+        }
+        break;
+      case PrepareMessage_t:
+        if ( (std::static_pointer_cast<PrepareMessage>(m))->b == state.p){
+          b_prepared +=1;
+        }
+        break;
+      }
+    }
+
+    if (b_prepared > node->quorumSet.threshold){
+      state.c = state.b;
+      node->SendMessage(Finish());
+    }
+  }
+
+  
 }
 void Slot::handle(std::shared_ptr<FinishMessage> msg) {
   printf("Finish");
